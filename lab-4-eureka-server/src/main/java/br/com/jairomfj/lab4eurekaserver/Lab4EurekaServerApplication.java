b7873ef64d7f518c796c3071460f4a6bc20ef1b2
Jairo Mendes <jairomfj@gmail.com>
2018-12-09 01:48:21 -0200

    eureka server, sentence and subject

---
+++ lab-4-eureka-server/src/main/java/br/com/jairomfj/lab4eurekaserver/Lab4EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package br.com.jairomfj.lab4eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Lab4EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4EurekaServerApplication.class, args);
+	}
+}
